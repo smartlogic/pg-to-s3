5c9ea3a88549e56b949d77797a9d60626be3d060
Dan Ivovich <dan@smartlogic.io>
2015-03-16 20:10:35 -0400

    use the running users creds for pg_dump and enable gpg encrypt

--- backup.rb
+++ backup.rb
@@ -3,10 +3,10 @@ require 'time'
 require 'aws-sdk'
 require 'fileutils'
 
-# .pgpass file required, it is in the following format
+# .pgpass file required if not connecting via peer/ident, it is in the following format
 #    hostname:port:database:username:password
-pg_user = ENV["POSTGRES_USERNAME"] || "postgres"
-pg_host = ENV["POSTGRES_HOST"] || "localhost"
+pg_user = ENV["POSTGRES_USERNAME"]
+pg_host = ENV["POSTGRES_HOST"]
 pg_port = ENV["POSTGRES_PORT"] || "5432"
 pg_database = ENV["POSTGRES_DATABASE"]
 
@@ -18,11 +18,23 @@ project_name = ENV["PROJECT_NAME"]
 time = Time.now.strftime("%Y-%m-%d")
 filename = "backup.#{Time.now.to_i}.#{time}.sql.dump"
 
-`pg_dump -Fc --username=#{pg_user} --no-password --host #{pg_host} --port #{pg_port} #{pg_database} > #{filename}`
+if pg_user && pg_host
+  `pg_dump -Fc --username=#{pg_user} --no-password --host #{pg_host} --port #{pg_port} #{pg_database} > #{filename}`
+elsif pg_host
+  `pg_dump -Fc --host #{pg_host} --port #{pg_port} #{pg_database} > #{filename}`
+else
+  `pg_dump -Fc #{pg_database} > #{filename}`
+end
 
 # verify file exists and file size is > 0 bytes
 unless File.exists?(filename) && File.new(filename).size > 0
   raise "Database was not backed up"
+end
+
+if ENV["ENCRYPT_TO"]
+  `gpg -r #{ENV["ENCRYPT_TO"]} -e #{filename}`
+  FileUtils.rm(filename)
+  filename = "#{filename}.gpg"
 end
 
 s3 = AWS.s3
